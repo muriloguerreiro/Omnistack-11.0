b5a4fd939945a0fe4cf151d2a8f023f2c0605d96
Murilo Guerreiro <guerreiro_murilo@hotmail.com>
2020-04-13 23:53:29 -0300

    Backend da aplicação

---
+++ backend/src/controllers/OngController.js
@@ -0,0 +1,43 @@
+const crypto = require('crypto');
+const connection = require('../database/connection');
+
+module.exports = {
+    async index(request, response) {
+        const ongs = await connection('ongs').select('*');
+    
+        return response.json(ongs);
+    },
+
+    async create(request, response) {
+        const { name, email, whatsapp, city, uf } = request.body;
+
+        const id = crypto.randomBytes(4).toString('HEX');
+
+        await connection('ongs').insert({
+            id,
+            name,
+            email,
+            whatsapp,
+            city,
+            uf,
+        });
+
+        return response.json({ id });
+    },
+
+    async delete(request, response) {
+        const { id } = request.params;
+        const ong_id = request.headers.authorization;
+
+        const ong = await connection('ongs')
+            .where('id', id)
+            .select('id')
+            .first();
+        if (ong.id != ong_id) {
+            return response.status(401).json({ error: 'Operation not allowed.' });
+        }
+        await connection('ongs').where('id', id).delete();
+
+        return response.status(204).send();
+    }
+};
